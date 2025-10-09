6fa352719b62580547010567b9e9021f4d191b3a
polsust <polsust@gmail.com>
2025-10-09 22:15:56 +0200

    Put defacto channel id in env

--- src/utils/getDefactoChannel.ts
+++ src/utils/getDefactoChannel.ts
@@ -1,10 +1,12 @@
 import { Client } from "discord.js"
 
-// const CHANNEL_ID = "244135020401393665" // The Sages
-const CHANNEL_ID = "587948791169024025" // Bot Testing
-
 export const getDefactoChannel = async (client: Client) => {
-  const channel = await client.channels.fetch(CHANNEL_ID, {
+  const defacto_channel_id = process.env.DEFACTO_CHANNEL_ID
+
+  if (!defacto_channel_id)
+    throw new Error("DEFACTO_CHANNEL_ID is missing in ENV")
+
+  const channel = await client.channels.fetch(defacto_channel_id, {
     force: true,
     allowUnknownGuild: true,
   })
